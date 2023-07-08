8c5ef05c79e33f2977322f192d098b94fd205bcd
Pasindu Sampath <104705509+pasindusampath@users.noreply.github.com>
2023-07-09 03:08:14 +0530

    Implemented Db To Get and search for Customer data within local Storage

--- db/Database.js
+++ db/Database.js
@@ -1,7 +1,22 @@
 const itemDb = "ITEMDATA"
+const customerDb = "CUSTOMERDATA"
 
-export function search(id){
+export function searchItem(id){
     let item = localStorage.getItem(itemDb);
     let parse = JSON.parse(item);
     return parse.find(obj=>obj._itemCode==id)
+}
+
+export function getAllItem(){
+    return JSON.parse(localStorage.getItem(itemDb));
+}
+
+export function searchCustomer(id){
+    let item = localStorage.getItem(customerDb);
+    let parse = JSON.parse(item);
+    return parse.find(obj=>obj._id==id)
+}
+
+export function getAllCustomers(){
+    return JSON.parse(localStorage.getItem(customerDb));
 }
