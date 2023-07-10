0654641fb708d7ad73431822e2c69a921e7bdde8
Pasindu Sampath <104705509+pasindusampath@users.noreply.github.com>
2023-07-11 01:50:16 +0530

    bug fixed

--- controller/DashBoardController.js
+++ controller/DashBoardController.js
@@ -22,8 +22,6 @@ export function getOrderCount(){
     $.ajax(setting).done(resp=>{
         $('#orderCount').text(resp)
         console.log(resp)
-    }).error(e=>{
-        setChart(["loading failed","loading failed"],[50,50],['rgb(54, 162, 235)', 'rgb(255, 205, 86)'])
     })
 }
 export function getCustomerCount(){
@@ -45,20 +43,27 @@ export function getIncomeData(){
     let months=[];
     let values=[];
     let setting = {
-        url:"http://localhost:8080/dashboard?switch=incomeData",
+        url:"http://localhost:808/dashboard?switch=incomeData",
         method:"GET",
         timeout:0
     }
 
-    $.ajax(setting).done(e=>{
-        console.log(e);
-        for (let i=0 ; i<e.length ; i++) {
-            months.push(e[i].month);
-            values.push(e[i].amount);
-            colors.push(color[i]);
-        }
-        setChart(months,values,colors);
-    })
+    try{
+        $.ajax(setting).done(e => {
+            console.log(e);
+            for (let i = 0; i < e.length; i++) {
+                months.push(e[i].month);
+                values.push(e[i].amount);
+                colors.push(color[i]);
+            }
+            setChart(months, values, colors);
+        }).fail((a,b,c)=>{
+            setChart(["loading failed", "loading failed"], [50, 50], ['rgb(54, 162, 235)', 'rgb(255, 205, 86)']);
+        })
+    }catch (e) {
+        setChart(["loading failed", "loading failed"], [50, 50], ['rgb(54, 162, 235)', 'rgb(255, 205, 86)']);
+    }
+
 
 
 }
