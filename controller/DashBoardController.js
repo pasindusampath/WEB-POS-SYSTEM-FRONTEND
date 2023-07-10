445f44088f2b927f3a147e2851686a112ac8bf1d
Pasindu Sampath <104705509+pasindusampath@users.noreply.github.com>
2023-07-11 01:34:26 +0530

    Added Functionality to display monthly income on pie chart

--- controller/DashBoardController.js
+++ controller/DashBoardController.js
@@ -1,14 +1,13 @@
 export class DashBoardController{
     constructor() {
         getOrderCount()
-        getCustomerCount()
+        getCustomerCount();
+        getIncomeData()
+        //this.setChart()
     }
 
 
 
-    getCustomerCount(){
-
-    }
 
 }
 new DashBoardController();
@@ -36,4 +35,49 @@ export function getCustomerCount(){
         $('#customerCount').text(resp)
         console.log(resp)
     })
+}
+
+export function getIncomeData(){
+    let color = ['rgb(255, 99, 132)', 'rgb(54, 162, 235)', 'rgb(255, 205, 86)', 'rgb(100, 200, 50)', 'rgb(0, 128, 255)', 'rgb(220, 20, 60)', 'rgb(128, 0, 128)', 'rgb(0, 0, 0)', 'rgb(255, 255, 0)', 'rgb(255, 165, 0)', 'rgb(70, 130, 180)', 'rgb(0, 128, 0)']
+    let colors = [];
+    let months=[];
+    let values=[];
+    let setting = {
+        url:"http://localhost:8080/dashboard?switch=incomeData",
+        method:"GET",
+        timeout:0
+    }
+
+    $.ajax(setting).done(e=>{
+        console.log(e);
+        for (let i=0 ; i<e.length ; i++) {
+            months.push(e[i].month);
+            values.push(e[i].amount);
+            colors.push(color[i]);
+        }
+        setChart(months,values,colors);
+    })
+
+
+}
+
+export function setChart(months,income,colors){
+    const chartElement = document.getElementById('myChart');
+    const chartData = {
+        labels: months,
+        datasets: [{
+            label: 'Income',
+            data: income,
+            backgroundColor: colors,
+            hoverOffset: 4
+        }]
+    };
+    const chartConfig = {
+        type: 'pie',
+        data: chartData,
+    };
+    console.log("months : "+months)
+    console.log("income : "+income)
+    console.log("Colors : "+ colors)
+    new Chart(chartElement, chartConfig)
 }
